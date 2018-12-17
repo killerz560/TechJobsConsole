e56942007c6b2f29a19e2dfd6cc55e03d59c78a7
Zach Zura <zacharyzura@hotmail.com>
2018-12-17 15:46:04 -0600

    Working code

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -42,7 +42,7 @@ namespace TechJobsConsole
                     {
                         List<string> results = JobData.FindAll(columnChoice);
 
-                        Console.WriteLine("\n*** All " + columnChoices[columnChoice] + " Values ***");
+                        Console.WriteLine("\n* All " + columnChoices[columnChoice] + " Values *");
                         foreach (string item in results)
                         {
                             Console.WriteLine(item);
@@ -63,7 +63,9 @@ namespace TechJobsConsole
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
+                        //Console.WriteLine("Search all fields not yet implemented.");
                     }
                     else
                     {
@@ -118,7 +120,25 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            //Console.WriteLine("printJobs is not implemented yet");
+            int count = someJobs.Count;
+            if (count == 0)
+            {
+                Console.WriteLine("No result");
+            }
+            else
+            {
+                foreach (Dictionary<string, string> item in someJobs)
+                {
+                    Console.WriteLine("\n*");
+                    foreach (KeyValuePair<string, string> itemline in item)
+                    {
+                        if (itemline.Key != "S.no")
+                            Console.WriteLine(itemline.Key + " : " + itemline.Value);
+                    }
+                    Console.WriteLine("*\n\n");
+                }
+            }
         }
     }
 }
